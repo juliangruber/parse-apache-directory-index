7367b3e413078798402f9773d6cf627a0a39d08e
Julian Gruber <julian@juliangruber.com>
2016-05-26 20:14:40 +0200

    add parsing of sizes. closes #1

--- example.js
+++ example.js
@@ -10,7 +10,7 @@ console.log(parse(`
            <h1>Index of /foo/bar</h1>
            <table><tr><th><img src="/icons/blank.gif" alt="[ICO]"></th><th><a href="?C=N;O=D">Name</a></th><th><a href="?C=M;O=A">Last modified</a></th><th><a href="?C=S;O=A">Size</a></th><th><a href="?C=D;O=A">Description</a></th></tr><tr><th colspan="5"><hr></th></tr>
            <tr><td valign="top"><img src="/icons/folder.gif" alt="[DIR]"></td><td><a href="beep/">beep/</a>           </td><td align="right">25-May-2016 11:53  </td><td align="right">  - </td><td>&nbsp;</td></tr>
-           <tr><td valign="top"><img src="/icons/folder.gif" alt="[DIR]"></td><td><a href="boop20160518/">boop20160518/</a>        </td><td align="right">19-May-2016 17:57  </td><td align="right">  - </td><td>&nbsp;</td></tr>
+           <tr><td valign="top"><img src="/icons/file.gif" alt="[]"></td><td><a href="boop20160518/">boop20160518/</a>        </td><td align="right">19-May-2016 17:57  </td><td align="right">  5.4M </td><td>&nbsp;</td></tr>
            <tr><td valign="top"><img src="/icons/folder.gif" alt="[DIR]"></td><td><a href="jazz20160518/">jazz20160518/</a>         </td><td align="right">19-May-2016 19:04  </td><td align="right">  - </td><td>&nbsp;</td></tr>
            <tr><td valign="top"><img src="/icons/folder.gif" alt="[DIR]"></td><td><a href="punk20160518/">punk20160518/</a>    </td><td align="right">19-May-2016 17:47  </td><td align="right">  - </td><td>&nbsp;</td></tr>
            <tr><td valign="top"><img src="/icons/folder.gif" alt="[DIR]"></td><td><a href="space20160518/">space20160518/</a>       </td><td align="right">19-May-2016 19:03  </td><td align="right">  - </td><td>&nbsp;</td></tr>
--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const cheerio = require('cheerio');
 
+const bytes = (str) => {
+  const m = /^\s*([0-9.]+)([A-Z]*)\s*$/.exec(str);
+  if (!m) return null;
+  const num = Number(m[1]);
+  const suf = m[2];
+  return suf == 'K' ? num * 1024
+    : suf == 'M' ? num * 1024 * 1024
+    : suf == 'G' ? num * 1024 * 1024 * 1024
+    : num;
+};
+
 module.exports = src => {
   const $ = cheerio.load(src);
   const dir = '/' + $('h1').text().split('/').slice(1).join('/');
@@ -14,9 +25,10 @@ module.exports = src => {
       name: path.slice(0, path.length - 1),
       path: `${dir}/${path}`,
       lastModified: new Date($tds.eq(2).text().trim()),
-      size: $tds.eq(3).text(),
+      size: bytes($tds.eq(3).text()),
       description: $tds.eq(4).text(),
     });
   });
   return { dir, files };
 }
+
