c848409178c4d0c3a368b47fd3a722ad8e4cf737
Edan Schwartz <edan@edanschwartz.com>
2016-08-26 15:06:23 -0500

    Add mocha test, against a basic index.html file

---
+++ test/spec/indexTest.js
@@ -0,0 +1,51 @@
+const parse = require('../../index');
+const fs = require('fs');
+const path = require('path');
+const assert = require('assert');
+
+describe('parseApacheDirectoryIndex', () => {
+
+  it('should parse an apache directory index', () => {
+    const indexHtmlPath = path.join(__dirname, '../fixtures/index.html');
+    const indexHtml = fs.readFileSync(indexHtmlPath, 'utf8');
+
+    assert.deepStrictEqual(parse(indexHtml), {
+      dir: '/foo/bar',
+      files: [
+        {
+          name: 'folderA/',
+          type: 'directory',
+          path: '/foo/bar/folderA/',
+          description: 'The very first folder',
+          size: null,
+          lastModified: new Date('25-May-2016 11:53')
+        },
+        {
+          name: 'folderB/',
+          type: 'directory',
+          path: '/foo/bar/folderB/',
+          size: 5662310.4,
+          lastModified: new Date('19-May-2016 17:57'),
+          description: ''
+        },
+        {
+          name: 'fileA.zip',
+          type: 'file',
+          path: '/foo/bar/fileA.zip',
+          size: 57344,
+          lastModified: new Date('28-May-2009 15:09'),
+          description: ''
+        },
+        {
+          name: 'fileB.zip',
+          type: 'file',
+          path: '/foo/bar/fileB.zip',
+          size: 662528,
+          lastModified: new Date('28-May-2009 17:32'),
+          description: ''
+        }
+      ]
+    })
+  });
+
+});
