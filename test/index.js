ff570e4b251567ded204c2337297882ecc82c53d
Edan Schwartz <eschwartz@aerisweather.net>
2016-09-21 09:50:16 -0500

    Parse header row, to determine column structure
    before, we were assuming that the columns were
    in a certain order.
    However, this may not always be the case
    - see http://ftp.wpc.ncep.noaa.gov/, for example,
      which is missing an `Icon` row
    
    The parsing script is more flexible now --
     it parses the header row first, to determine the column ordering,
     and then parses each column using that same ordering

--- test/index.js
+++ test/index.js
@@ -20,7 +20,7 @@ test('should parse apache directory index files', t => {
         name: 'folderB/',
         type: 'directory',
         path: '/foo/bar/folderB/',
-        size: 5662310.4,
+        size: null,
         lastModified: new Date('19-May-2016 17:57'),
         description: ''
       },
@@ -28,7 +28,7 @@ test('should parse apache directory index files', t => {
         name: 'fileA.zip',
         type: 'file',
         path: '/foo/bar/fileA.zip',
-        size: 57344,
+        size: 5662310.4,
         lastModified: new Date('28-May-2009 15:09'),
         description: ''
       },
@@ -39,6 +39,49 @@ test('should parse apache directory index files', t => {
         size: 662528,
         lastModified: new Date('28-May-2009 17:32'),
         description: ''
+      }
+    ]
+  });
+  t.end();
+});
+
+test('should parse apache directory index files (alt format)', t => {
+  const index = fs.readFileSync(`${__dirname}/fixture/apache-index-alt.html`, 'utf8');
+
+  t.deepEqual(parse(index), {
+    dir: '/foo/bar',
+    files: [
+      {
+        name: 'folderA/',
+        type: 'directory',
+        path: '/foo/bar/folderA/',
+        description: null,
+        size: null,
+        lastModified: new Date('20-Sep-2016 20:33')
+      },
+      {
+        name: 'folderB/',
+        type: 'directory',
+        path: '/foo/bar/folderB/',
+        size: null,
+        lastModified: new Date('20-Sep-2016 20:33'),
+        description: null,
+      },
+      {
+        name: 'fileA.zip',
+        type: 'file',
+        path: '/foo/bar/fileA.zip',
+        size: 57344,
+        lastModified: new Date('05-Sep-2016 20:41'),
+        description: null
+      },
+      {
+        name: 'fileB.zip',
+        type: 'file',
+        path: '/foo/bar/fileB.zip',
+        size: 662528,
+        lastModified: new Date('06-Sep-2016 06:14'),
+        description: null
       }
     ]
   });
